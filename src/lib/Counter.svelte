5766527b3764d624fa89b680367254c9ffe791bb
ReactiveX22 <akibreactive@gmail.com>
2024-11-05 19:08:41 +0600

    Initial commit

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,11 @@
+<script>
+  let count = $state(0);
+  const increment = () => {
+    count += 1;
+  };
+</script>
+
+<div>
+  <p>Count: {count}</p>
+  <button onclick={increment}>+1</button>
+</div>
